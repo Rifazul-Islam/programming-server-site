81b2c16095115f79d85237f727b9d3f3ae9ccaaf
Rifazul <rifazul50@gmail.com>
2022-10-26 04:43:30 +0600

    add courses data load

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000 ;
+
+app.use(cors())
+
+ const courses = require('./data/Courses.json')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+  app.get('/courses',(req,res)=>{
+
+       res.send(courses)
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
